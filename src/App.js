aa4b8ef4aedaa1804f428d50f1416437af5ce466
Derryl999 <derryl.aranha999@gmail.com>
2020-09-18 19:36:39 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Navbar,NavbarBrand} from 'reactstrap'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <Navbar dark color="primary">
+     <div className="container">
+       <NavbarBrand href="/">Resort</NavbarBrand>
+     </div>
+   </Navbar>
+    </div>
+  );
+}
+
+export default App;
